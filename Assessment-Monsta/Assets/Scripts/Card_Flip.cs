8f575a70ebe53642203dd8bd071ce5dbd7cfb741
SelinaLim <escales1018@gmail.com>
2022-08-12 19:12:39 +0800

    Flip card

---
+++ Assessment-Monsta/Assets/Scripts/Card_Flip.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Card_Flip : MonoBehaviour, IPointerDownHandler
+{
+    public Image image;
+    public Sprite front, back;
+
+    [SerializeField] private bool isFaceUp, enabledCoroutine;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        isFaceUp = false;
+        enabledCoroutine = true;
+
+        //StartCoroutine(ShuffleCard());
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    //Functions
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " Was Clicked.");
+        if(enabledCoroutine)
+        {
+            StartCoroutine(RotateCard());
+        }
+    }
+
+
+    //IEnumerator
+    public IEnumerator ShuffleCard()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+
+    }
+
+    public IEnumerator RotateCard()
+    {
+
+        if(isFaceUp == false)
+        {
+            for(float i = 0f; i <= 180f; i+=10f)
+            {
+                transform.rotation = Quaternion.Euler(0f, i, 0f);
+
+                if(i == 90f) //flip 90 degree, change to front sprite
+                {
+                    image.sprite = front;
+                }
+                yield return new WaitForSeconds(0.02f);
+
+            }
+
+        }
+        else
+        {
+            for (float i = 180f; i >= 0f; i-=10f)
+            {
+                transform.rotation = Quaternion.Euler(0f, i, 0f);
+
+                if (i == 90f) //flip 90 degree, change to back sprite
+                {
+                    image.sprite = back;
+                }
+                yield return new WaitForSeconds(0.02f);
+
+            }
+        }
+
+        //enabledCoroutine = true;
+        //isFaceUp = false;
+    }
+}
---
+++ Assessment-Monsta/Assets/Scripts/Card_Spawn.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card_Spawn : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
